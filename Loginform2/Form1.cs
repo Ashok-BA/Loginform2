db69409c23bf066d573178341a477eadd6be99f4
Ashok-BA <ashok972001@outlook.com>
2026-06-23 10:21:23 +0530

    update chsnges

--- Loginform2/Form1.cs
+++ Loginform2/Form1.cs
@@ -20,36 +20,53 @@ namespace Loginform2
 
         private void btnsubmit_Click(object sender, EventArgs e)
         {
-            string cs = "Server=ASHOK\\SQLEXPRESS; Database=adonet; Integrated Security=true";
-            SqlConnection con = new SqlConnection(cs);
-            con.Open();
-            SqlCommand cmd = new SqlCommand("insert into logininfo values('" + txtusername.Text + "','" + txtpassword.Text + "')", con);
-            cmd.ExecuteNonQuery();
-            txtusername.Clear();
-            txtpassword.Text = "";
-            lbldisplay.Text = "Inserted Successfully";
-            lbldisplay.ForeColor = System.Drawing.Color.Green;
-            con.Close();
+            try
+            {
 
+
+                string cs = "Server=ASHOK\\SQLEXPRESS; Database=adonet; Integrated Security=true";
+                SqlConnection con = new SqlConnection(cs);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into logininfo values('" + txtusername.Text + "','" + txtpassword.Text + "')", con);
+                cmd.ExecuteNonQuery();
+                txtusername.Clear();
+                txtpassword.Text = "";
+                lbldisplay.Text = "Inserted Successfully";
+                lbldisplay.ForeColor = System.Drawing.Color.Green;
+                con.Close();
+            }
+            catch(Exception)
+            {
+                lbldisplay.Text = "Please valid Input Details";
+            }
 
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            string cs = "Server=ASHOK\\SQLEXPRESS; Initial Catalog=adonet; Trusted_Connection=true";
-            SqlConnection con = new SqlConnection(cs);
-            con.Open();
-            SqlCommand cmd = new SqlCommand("update logininfo set password='"+txtpassword.Text+"'where username='"+txtusername.Text+"'", con);
-            cmd.ExecuteNonQuery();
-            txtusername.Clear();
-            txtpassword.Text = "";
-            lbldisplay.Text = "Updated Successfully";
-            lbldisplay.ForeColor = System.Drawing.Color.Blue;
-            con.Close();
+            try
+            {
+                string cs = "Server=ASHOK\\SQLEXPRESS; Initial Catalog=adonet; Trusted_Connection=true";
+                SqlConnection con = new SqlConnection(cs);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("update logininfo set password='" + txtpassword.Text + "'where username='" + txtusername.Text + "'", con);
+                cmd.ExecuteNonQuery();
+                txtusername.Clear();
+                txtpassword.Text = "";
+                lbldisplay.Text = "Updated Successfully";
+                lbldisplay.ForeColor = System.Drawing.Color.Blue;
+                con.Close();
+            }
+            catch(Exception)
+            {
+                lbldisplay.Text = "Please Enter valid updatable Details";
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
                 string cs = "Server=ASHOK\\SQLEXPRESS; Database=adonet; Integrated Security=true";
                 SqlConnection con = new SqlConnection(cs);
                 con.Open();
@@ -60,19 +77,31 @@ namespace Loginform2
                 txtpassword.Text = "";
                 lbldisplay.Text = "Deleted  Successfully";
                 lbldisplay.ForeColor = System.Drawing.Color.Red;
-                con.Close();           
+                con.Close();
+            }
+            catch(Exception)
+            {
+                lbldisplay.Text = "Delation of Data is Invalid";
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string cs = "Server=ASHOK\\SQLEXPRESS; Initial Catalog=adonet; Integrated Security=true";
-            SqlConnection con = new SqlConnection(cs);
-            SqlDataAdapter da = new SqlDataAdapter("select * from logininfo", con);
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-            lbldisplay.Text = "Displaying Data in GridView";
-            lbldisplay.ForeColor = System.Drawing.Color.Purple;
-            datagridview1.DataSource = dt;            
+            try
+            {
+                string cs = "Server=ASHOK\\SQLEXPRESS; Initial Catalog=adonet; Integrated Security=true";
+                SqlConnection con = new SqlConnection(cs);
+                SqlDataAdapter da = new SqlDataAdapter("select * from logininfo", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                lbldisplay.Text = "Displaying Data in GridView";
+                lbldisplay.ForeColor = System.Drawing.Color.Purple;
+                datagridview1.DataSource = dt;
+            }
+            catch(Exception)
+            {
+                lbldisplay.Text = "Enter proper valid Button";
+            }
         }
     }
 }
